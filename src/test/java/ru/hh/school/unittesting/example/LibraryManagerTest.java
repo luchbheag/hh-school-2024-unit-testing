4dd61a7c2e4aacf70a946271d587a146a1ea9b5a
luchbheag <nedolivko.julia@gmail.com>
2024-12-27 19:14:06 +0100

    Add tests for borrowBook() method

--- src/test/java/ru/hh/school/unittesting/example/LibraryManagerTest.java
+++ src/test/java/ru/hh/school/unittesting/example/LibraryManagerTest.java
@@ -9,7 +9,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.hh.school.unittesting.homework.LibraryManager;
 import ru.hh.school.unittesting.homework.NotificationService;
 import ru.hh.school.unittesting.homework.UserService;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class LibraryManagerTest {
@@ -41,17 +45,53 @@ public class LibraryManagerTest {
         assertEquals(0, libraryManager.getAvailableCopies(bookName));
     }
 
+    @Test
+    void borrowBookWhenUserIsNotActive() {
+        final String userId = "test_user";
+        final String bookId = "test_book";
+        libraryManager.addBook(bookId, 5);
+        when(userService.isUserActive(userId)).thenReturn(false);
 
+        assertFalse(libraryManager.borrowBook(bookId, userId));
+        verify(notificationService).notifyUser(userId, "Your account is not active.");
+        verifyNoMoreInteractions(notificationService);
+    }
+
+    @Test
+    void borrowBookWhenThereIsNoAvailableCopies() {
+        final String userId = "test_user";
+        final String bookId = "test_book";
+        libraryManager.addBook(bookId, 0);
+        when(userService.isUserActive(userId)).thenReturn(true);
+
+        assertFalse(libraryManager.borrowBook(bookId, userId));
+        verifyNoInteractions(notificationService);
+    }
+
+    @Test
+    void borrowBookWhenThereAreAvailableCopies() {
+        final String userId = "test_user";
+        final String bookId = "test_book";
+        libraryManager.addBook(bookId, 5);
+        when(userService.isUserActive(userId)).thenReturn(true);
+
+        assertTrue(libraryManager.borrowBook(bookId, userId));
+        assertEquals(4, libraryManager.getAvailableCopies(bookId));
+        verify(notificationService).notifyUser(userId, "You have borrowed the book: " + bookId);
+        verifyNoMoreInteractions(notificationService);
+    }
 
 
     /*
-    - void addBook(String bookId, int quantity)
     - boolean borrowBook(String bookId, String userId)
     - boolean returnBook(String bookId, String userId)
-    - int getAvailableCopies(String bookId)
     - double calculateDynamicLateFee(int overdueDays,
                                     boolean isBestseller,
                                     boolean isPremiumMember)
+
+    ****
+    - void addBook(String bookId, int quantity)
+    - int getAvailableCopies(String bookId)
 
      */
 }
